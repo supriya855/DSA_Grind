823dd564644c0ccdd92edcd6c46b6c9b9ee029d3
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-04-28 15:58:38 +0530

    Update 226. Invert Binary Tree.java

--- Trees/226. Invert Binary Tree.java
+++ Trees/226. Invert Binary Tree.java
@@ -72,3 +72,13 @@ invertTree(4)
             return root;
     }
 }
+***********************************
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) return null;
+        TreeNode left =root.left;
+        root.left=invertTree(root.right);
+        root.right=invertTree(left);
+        return root;
+    }
+}
