529fb4ce7cc07d6730d562c7e4d908162a13ae44
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-05-06 23:59:36 +0530

    Update 1026. Maximum Difference Between Node and Ancestor.java

--- Trees/1026. Maximum Difference Between Node and Ancestor.java
+++ Trees/1026. Maximum Difference Between Node and Ancestor.java
@@ -10,3 +10,26 @@ class Solution {
     }
 }
 // while traversing recursively from root to leaf we are finding max and min values of root 
+********************************************************************************************************
+class Solution {
+    int max=0;
+    public int maxAncestorDiff(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        path(root,list);
+        return max;
+    }
+    public void path(TreeNode root,List<Integer> list){
+        if(root==null) return;
+        list.add(root.val);
+        if(root.left==null&&root.right==null){
+            for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++){
+                max=Math.max(max,(int)Math.abs(list.get(i)-list.get(j)));
+            }
+        }
+        }
+        path(root.left,list);
+        path(root.right,list);
+        list.remove(list.size()-1);
+    } 
+}
