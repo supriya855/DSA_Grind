66fec64c9dcaa6b2a5200dc8049e246f6f540ee6
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-11-27 22:19:01 +0530

    Update 125. Valid Palindrome.java

--- Two Pointers/125. Valid Palindrome.java
+++ Two Pointers/125. Valid Palindrome.java
@@ -36,3 +36,30 @@ class Solution {
 
 }
 // its time complexity O(length of string)=>O(N)
+
+(OR)
+_____________________________________________________________________________________
+
+  
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start=0;
+        int end=s.length()-1;
+        while(start<=end){
+            char firstChar=s.charAt(start);
+            char lastChar=s.charAt(end);
+            if(!Character.isLetterOrDigit(firstChar)) 
+            start++;
+            else if(!Character.isLetterOrDigit(lastChar)) 
+            end--;
+            else {
+            if(Character.toLowerCase(firstChar)!=Character.toLowerCase(lastChar)) return false;
+
+            start++;
+            end--;
+            }
+
+        }
+        return true;
+    }
+}
