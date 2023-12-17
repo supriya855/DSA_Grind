31043f280943370d0dec481388ba5bb7928ba1e5
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-12-17 15:03:24 +0530

    Update RightViewBT.java

--- Trees/RightViewBT.java
+++ Trees/RightViewBT.java
@@ -1,3 +1,44 @@
+//BRUTE FORce using BFS
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> q =new LinkedList<>();
+        if(root==null) return res;
+        q.add(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+             List<Integer> l = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                if(q.peek().left!=null){
+                    q.add(q.peek().left);
+                }
+                  if(q.peek().right!=null){
+                    q.add(q.peek().right);
+                }
+               l.add(q.remove().val); 
+            }
+            res.add(l.get(l.size()-1));
+        }
+        return res;
+    }
+}
+// DFS
+
 class Solution {
     List<Integer> l = new ArrayList<>();
     public List<Integer> rightSideView(TreeNode root) {     
