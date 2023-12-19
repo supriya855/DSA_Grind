91ef2b6d1aa6fad5b2f20eb5997c5adc3856d66c
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-12-19 23:29:51 +0530

    Update 437. Path Sum III .java

--- Trees/437. Path Sum III .java
+++ Trees/437. Path Sum III .java
@@ -27,3 +27,47 @@ class Solution {
     return (cur == (long)sum ? 1 : 0) + dfs(root.left, sum, cur) + dfs(root.right, sum, cur);
   }
 }
+=================================================================================================
+   /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        HashMap<Long,Long> map = new HashMap<>();
+        map.put((long)0,(long)1);
+        helper(root,0,targetSum,map);
+        return count;
+    }
+    int count=0;
+    public void helper(TreeNode root,long currSum,int targetSum,HashMap<Long,Long> map){
+        if(root==null) return;
+        currSum+=(long)root.val;
+        if(map.containsKey(currSum-targetSum)){
+            count+=map.get(currSum-targetSum);
+        }
+        if(!map.containsKey(currSum)){
+            map.put(currSum,(long)1);
+        }
+        else{
+            map.put(currSum,map.get(currSum)+1);
+        }
+        helper(root.left,currSum,targetSum,map);
+        helper(root.right,currSum,targetSum,map);
+        map.put(currSum,map.get(currSum)-1);
+
+
+    }
+}
+//Dooo DRYYYY lottt
