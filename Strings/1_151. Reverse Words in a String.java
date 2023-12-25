6cd1d3c131f7a57cb69b020e8cd656184c0760c1
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-12-25 22:56:50 +0530

    Update 1_151. Reverse Words in a String.java

--- Strings/1_151. Reverse Words in a String.java
+++ Strings/1_151. Reverse Words in a String.java
@@ -1,3 +1,5 @@
+==============================================================
+// it took 20sec
 class Solution {
     public String reverseWords(String s) {
         List<String> l = new ArrayList<>();
@@ -28,4 +30,47 @@ class Solution {
         }
         return res;
     }
+}
+=========================================================================
+    class Solution {
+    public String reverseWords(String s) {
+        if(s==null) return s;
+        char[] ch = s.toCharArray();
+        int n= ch.length;
+        // step-1: reverse the whole string
+        reverse(ch,0,n-1);
+        //step-2:reverse each word
+        reverseword(ch,n);
+        //step-3: to remove extra spaces
+        return cleanSpace(ch,n);
+    }
+    public void reverseword(char[] ch,int n){
+        int i=0,j=0;
+            while(i<n){
+                while(i<j||(i<n&&ch[i]==' ')) i++;//counting space
+                while(j<i||(j<n&&ch[j]!=' ')) j++;// counting non space ones
+                reverse(ch,i,j-1);// so from after space to before the space of every word we are reverseing it
+            }
+    }
+    public void reverse(char[] ch,int s,int e){
+        while(s<e){
+            char temp=ch[s];
+            ch[s]=ch[e];
+            ch[e]=temp;
+            s++;
+            e--;
+        }
+       //  System.out.println(Arrays.toString(ch));
+    }
+    public String cleanSpace(char[] ch ,int n){
+        int i=0,j=0;
+        while(i<n){
+            while(i<n&&ch[i]==' ') i++;// skip the space 
+            while(i<n&&ch[i]!=' ') ch[j++]=ch[i++];// keeping every non space one
+            while(i<n&&ch[i]==' ') i++;
+        if(i<n) ch[j++]=' ';//assining only one space
+        }
+        return new String(ch).substring(0,j);
+    }
+
 }
