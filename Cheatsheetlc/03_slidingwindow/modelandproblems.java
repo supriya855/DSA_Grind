e99a67cb4adac67dc7c7c615edf2f098da71630c
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2024-02-18 23:20:46 +0530

    Added a problem1

--- Cheatsheetlc/03_slidingwindow/modelandproblems.java
+++ Cheatsheetlc/03_slidingwindow/modelandproblems.java
@@ -15,3 +15,25 @@ public int fn(int[] arr) {
 
     return ans;
 }
+class Solution{
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
+        // code here
+    long ans=0,curr=0;
+    int left = 0;
+
+    for (int right = 0; right < Arr.size(); right++) {
+        // do logic here to add arr[right] to curr
+        curr+=Arr.get(right);
+        
+        if (right-left+1==K) {//WINDOW_CONDITION_BROKEN
+            // remove arr[left] from curr
+            ans=Math.max(ans,curr);//
+            curr=curr-Arr.get(left);
+            left++;
+        }
+        
+    }
+
+    return ans;
+}
+}
