fde2020701c693f679190879ba581223a93caec2
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2024-06-05 19:39:59 +0530

    Create Basics.java

---
+++ Java/Basics.java
@@ -0,0 +1,25 @@
+Abstraction can be achived 100% by using interface (Showing the neccessary information)
+Encapsulation can be achieved by declaring all the variables in a class as private and writing public methods in the class to set and get the values of the variables.
+(encapsulation can also interchangebly used for Data hiding)
+INHERITANCE
+//base class or parent class or super class
+class A{
+  //parent class methods
+  void method1(){}
+  void method2(){}
+}
+
+//derived class or child class or base class
+class B extends A{  //Inherits parent class methods
+  //child class methods
+  void method3(){}
+  void method4(){}
+}
+
+ The ability to appear in many forms is called polymorphism.
+Note: Polymorphism in Java is mainly of 2 types: 
+
+Overloading => 
+Overriding  => 
+A wrapper class wraps (encloses) around a data type and gives it an object appearance. Wrapper classes are final and immutable. Two concepts are there in the wrapper classes namely autoboxing and unboxing. 
+
