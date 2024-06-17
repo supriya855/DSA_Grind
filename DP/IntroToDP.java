cb7e5d17e9a7690cc36b69bbcb0479a95cf249a3
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2024-06-17 09:43:41 +0530

    Update IntroToDP.java

--- DP/IntroToDP.java
+++ DP/IntroToDP.java
@@ -14,7 +14,7 @@ Let's start with fibonacci recursion
   if(n<=1) return n;
   return f(n-1)+f(n-2)
     }
-Using DP
+Using DP Memorization
 -------
 int[] dp = new int[n+1];
 Arrays.fill(dp,-1);
@@ -24,3 +24,19 @@ f(dp,n){
   if(dp[n]!=-1) return dp[n];
   return dp[n]=dp[n-1]+dp[n-1];
 }
+  Using Tabulation
+    ==============
+  int[] dp = new int[n+1];
+dp[0] =0, dp[1]=1;
+  for(int i=2;i<=n;i++){
+    dp[i]=dp[i-1]+dp[i-2];
+  }
+  return dp[n];
+int prev2 =0, prev1=1;
+  for(int i=2;i<=n;i++){
+    int curri = prev1+prev2;
+    prev2= prev1;
+    curri=prev1;
+  }
+  Sout(prev1);
+  
