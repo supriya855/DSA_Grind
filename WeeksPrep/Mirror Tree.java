906f9f309c4c534a32b66e681d55289612fadc70
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2024-06-22 19:37:46 +0530

    Create Mirror Tree.java

---
+++ WeeksPrep/Mirror Tree.java
@@ -0,0 +1,27 @@
+
+Whenever saw the question just imagine we have to swap just by loooking we can able get it 
+think of level order and node swaps 
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        // Your code here
+        Queue<Node> q = new LinkedList();
+        q.add(node);
+        while(!q.isEmpty()){
+            Node popNode  = q.poll();
+                if(popNode.left!=null){
+                q.add(popNode.left);
+                }
+                if(popNode.right!=null){
+                q.add(popNode.right);
+                }
+                Node temp = popNode.left;
+                    popNode.left = popNode.right;
+                    popNode.right = temp;
+          // this part of swap above also we can add before adding into queue
+               // here it is same as level order but no for loop requore theere we need for every level to segregare
+               // here we just need every individual node
+        }
+    }
+}
