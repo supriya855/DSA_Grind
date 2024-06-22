5b179d628b95d24633f5b104ac6c0d0e2c400379
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2024-06-22 08:47:28 +0530

    Update TopologicalSort.java

--- Graph/TopologicalSort.java
+++ Graph/TopologicalSort.java
@@ -1,3 +1,40 @@
+TOPSORT USING DFS => JUST DAG TRAVERSAL
+we are storing all element in stack while backtrack we can psuh to stack at the end we can pop get all elements
+
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // add your code here
+        int[] vis = new int[V];
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<V;i++){
+            if(vis[i]==0){
+                dfs(i,vis,adj, st);
+            }
+        }
+        int[] res = new int[V];
+        int idx=0;
+        while(!st.isEmpty()){
+            res[idx++] = st.pop();
+        }
+        return res;
+    }
+    public static void dfs(int i, int[] vis,ArrayList<ArrayList<Integer>> adj,Stack<Integer> st){
+        vis[i]=1;
+        for(int itr : adj.get(i)){
+            if(vis[itr]==0){
+                vis[itr]=1;
+                dfs(itr,vis,adj,st);
+                
+            }
+        }
+        st.push(i);
+    }
+}
+
+
 class Solution
 {
     //Function to return list containing vertices in Topological order. 
