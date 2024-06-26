ba0bd7e4d3b9d9af19a43f2e842e74c6ddcdbb8e
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2024-06-26 16:21:57 +0530

    Create First Index occurent of element.java

---
+++ WeeksPrep/First Index occurent of element.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+int[] arr = new int[n];
+int x = 14;
+for(int i=0;i<n;i++){
+    arr[i]=sc.nextInt();
+}
+ int res = findFirstOccurence(arr,x);
+ System.out.println(res);
+    }
+
+public static int findFirstOccurence(int[] arr, int x){
+int s=0, e= arr.length-1;
+boolean isFlag = false;
+if(arr[s]==x) return s;
+    while(s<=e){
+        int mid = (s+(e-s)/2);
+        if(arr[mid]==x){
+              isFlag = true;
+            if((mid-1>0 && mid-1<arr.length-1 && arr[mid-1]!=x) || s==e){
+                // System.out.println(mid);
+                return mid;
+            }
+            else{
+                e= mid;
+            }
+          
+        }
+        else if(arr[mid]<x) s= mid+1;
+        else e = mid;
+        
+    }
+    if(isFlag)
+    return s;
+    else // if X is not present
+    return -1;
+  }
+}
