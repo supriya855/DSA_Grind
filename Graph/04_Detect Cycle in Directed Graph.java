cd11060e26dc055bb3df0f64209874ce9c9c1d71
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2024-06-27 09:30:49 +0530

    Update 04_Detect Cycle in Directed Graph.java

--- Graph/04_Detect Cycle in Directed Graph.java
+++ Graph/04_Detect Cycle in Directed Graph.java
@@ -41,3 +41,41 @@ class Solution {
         
     }
 }
+
+We can also use topo sort as we topo sort is applicable for DIrected ascyclic graph 
+if topo sort is failing means there is a cycle
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int[] indegree = new int[V];
+        // add your code here
+        for(int i=0;i<V;i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+     int count = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            count++;
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.add(it);
+                }
+            }
+        }
+        return count!=V;// this how we are verifyinh
+    }
+}
