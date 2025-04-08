8e2a34475938d58268dca5fd7a51515e01b8f091
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2025-04-08 08:20:47 +0530

    Update Sliding window maximum.java

--- SlidingWindow/Sliding window maximum.java
+++ SlidingWindow/Sliding window maximum.java
@@ -26,3 +26,33 @@ class Solution {
        return res;
     }
 }
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   // Amortized : O(N)
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums==null || k <=0) return new int[0];
+   Deque<Integer> dq = new ArrayDeque<>();
+   int[] res = new int[nums.length-k+1];
+   int idx = 0;
+   for(int i=0;i<nums.length;i++){
+   // exceeds window range means pop it
+
+        while(!dq.isEmpty() && dq.peek()<i-k+1){
+            dq.poll();
+        }
+
+          while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
+            dq.pollLast();
+        }
+
+        dq.offer(i);
+        if(i>=k-1){
+            res[idx++]= nums[dq.peek()];
+        }
+   }
+   return res;
+
+    }
+}
+// TC : O(N) SC : O(N)
